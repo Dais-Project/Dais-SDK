3a4b24efc916b85acd3e356dd36be203433c0d38
BHznJNs <441768875@qq.com>
2025-12-26 11:55:37 +0800

    extracted param parsing logic into independent class

--- src/liteai_sdk/__init__.py
+++ src/liteai_sdk/__init__.py
@@ -22,6 +22,7 @@ from litellm.types.utils import LlmProviders,\
                                 ModelResponseStream as LiteLlmModelResponseStream,\
                                 Choices as LiteLlmModelResponseChoices
 from .debug import enable_debugging
+from .param_parser import ParamParser
 from .stream import AssistantMessageCollector
 from .tool import ToolFn, ToolDef, RawToolDef, prepare_tools
 from .tool.execute import execute_tool_sync, execute_tool, parse_arguments
@@ -58,36 +59,7 @@ class LLM:
         self.provider = provider
         self.base_url = base_url
         self.api_key = api_key
-
-    def _parse_params_nonstream(self, params: LlmRequestParams):
-        tools = params.tools and prepare_tools(params.tools)
-        return {
-            "model": f"{self.provider.value}/{params.model}",
-            "messages": [message.to_litellm_message() for message in params.messages],
-            "base_url": self.base_url,
-            "api_key": self.api_key,
-            "tools": tools,
-            "tool_choice": params.tool_choice,
-            "stream": False,
-            "timeout": params.timeout_sec,
-            "extra_headers": params.headers,
-            **(params.extra_args or {})
-        }
-
-    def _parse_params_stream(self, params: LlmRequestParams):
-        tools = params.tools and prepare_tools(params.tools)
-        return {
-            "model": f"{self.provider.value}/{params.model}",
-            "messages": [message.to_litellm_message() for message in params.messages],
-            "base_url": self.base_url,
-            "api_key": self.api_key,
-            "tools": tools,
-            "tool_choice": params.tool_choice,
-            "stream": True,
-            "timeout": params.timeout_sec,
-            "extra_headers": params.headers,
-            **(params.extra_args or {})
-        }
+        self._param_parser = ParamParser(self.provider, self.base_url, self.api_key)
 
     @staticmethod
     def _should_resolve_tool_calls(
@@ -177,7 +149,7 @@ class LLM:
             api_key=self.api_key)
 
     def generate_text_sync(self, params: LlmRequestParams):
-        response = completion(**self._parse_params_nonstream(params))
+        response = completion(**self._param_parser.parse_nonstream(params))
         response = cast(LiteLlmModelResponse, response)
         choices = cast(list[LiteLlmModelResponseChoices], response.choices)
         message = choices[0].message
@@ -189,7 +161,7 @@ class LLM:
         return result
 
     async def generate_text(self, params: LlmRequestParams) -> GenerateTextResponse:
-        response = await acompletion(**self._parse_params_nonstream(params))
+        response = await acompletion(**self._param_parser.parse_nonstream(params))
         response = cast(LiteLlmModelResponse, response)
         choices = cast(list[LiteLlmModelResponseChoices], response.choices)
         message = choices[0].message
@@ -217,7 +189,7 @@ class LLM:
                     full_message_queue.put(tool_message)
             full_message_queue.put(None)
 
-        response = completion(**self._parse_params_stream(params))
+        response = completion(**self._param_parser.parse_stream(params))
         message_collector = AssistantMessageCollector()
         returned_stream = stream(cast(CustomStreamWrapper, response))
         full_message_queue = queue.Queue[AssistantMessage | ToolMessage | None]()
@@ -241,7 +213,7 @@ class LLM:
                     await full_message_queue.put(tool_message)
             await full_message_queue.put(None)
 
-        response = await acompletion(**self._parse_params_stream(params))
+        response = await acompletion(**self._param_parser.parse_stream(params))
         message_collector = AssistantMessageCollector()
         returned_stream = stream(cast(CustomStreamWrapper, response))
         full_message_queue = asyncio.Queue[AssistantMessage | ToolMessage | None]()
